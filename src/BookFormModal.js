9daee07d331588a7d96efc2fe288c61f30ddff05
K1ng-T0ast <kylejwhite@proton.me>
2023-04-25 14:54:56 -0700

    create basic modal and post/delete structures

---
+++ src/BookFormModal.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Modal } from 'react-bootstrap'
+
+class BookModal extends React.Component {
+    render() {
+        return(
+            <>
+            <Modal></Modal>
+            </>
+        )
+    }
+}
+
+export default BookModal;
